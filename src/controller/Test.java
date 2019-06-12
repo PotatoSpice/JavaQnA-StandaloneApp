bea3e27aeaa0a6e1a797edb299927872af7408e7
JoseBaltar <josebaltar_17@hotmail.com>
2019-06-12 16:46:54 +0100

    [Update] Bastante desenvolvimento na classe 'Test'. No entanto, ainda não se chegou a uma conclusão para a forma em como será utilizada a estrutura de dados.

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -1,48 +1,220 @@
+/*
+ * Nome: José Ribeiro Baltar
+ * Número: 8170212
+ * Turma: Turma 1
+ * 
+ * Nome: Rodrigo Alexandre Ferreira Coelho
+ * Número: 8170282
+ * Turma: Turma 1
+ */
 package controller;
 
+import com.google.gson.Gson;
 import interfaces.controller.ITestStatistics;
 import interfaces.exceptions.TestException;
 import interfaces.models.IQuestion;
+import models.Question;
 
-public class ITest implements interfaces.controller.ITest {
+/**
+ * <b>Esta classe implementa todos os métodos definidos no contrato relativo,
+ * presente na API 'recursos.jar'. Ou seja, a documentação para cada 'overriden
+ * method' encontra-se já especificada na documentação da API.</b>
+ * Contudo, novos métodos adicionados ou alterações pertinentes serão
+ * devidamente documentadas.
+ */
+public class Test implements interfaces.controller.ITest {
+
+    private Question[] questions;
+    private int data_size = 50;
+    private TestStatistics statistics = null;
+
+    /**
+     * Construtor para inicialização da estrutura de dados com tamanho inicial
+     * por defeito.
+     */
+    public Test() {
+        this.questions = new Question[this.data_size];
+    }
 
     @Override
-    public boolean addQuestion(IQuestion var1) throws TestException {
+    public boolean addQuestion(IQuestion q) throws TestException {
+        if (q == null)
+        { // Lança uma exceção se o objeto for nulo
+            throw new TestException();
+        }
+
+        int pos = 0;
+        while (pos < questions.length)
+        { // Iterar sobre a estrutura de dados
+            if (questions[pos] == null)
+            { // Insere o objeto quando encontrar uma posição vazia
+                questions[pos] = (Question) q;
+                return true;
+            }
+            pos++;
+        }
         return false;
     }
 
     @Override
-    public IQuestion getQuestion(int var1) throws TestException {
+    public IQuestion getQuestion(int pos) throws TestException {
+        if (pos >= questions.length || pos < 0)
+        { // verificar se a posição especificada pode ser verificada
+            if (questions[pos] != null)
+            { // retorna o objeto se existir
+                return questions[pos];
+            } else
+            { // se o objeto não existe lança uma exceção
+                throw new TestException("Posição especificada não contém nenhuma "
+                        + "Questão!");
+            }
+        }
         return null;
     }
 
     @Override
-    public boolean removeQuestion(int var1) {
+    public boolean removeQuestion(int pos) {
+        if (pos > 0 && pos < questions.length && questions[pos] != null)
+        { // verificar se a posição pode ser verificada e se o conteúdo existe
+            questions[pos] = null;
+            // trim(pos);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean removeQuestion(IQuestion var1) {
-        return false;
+    public boolean removeQuestion(IQuestion q) {
+
+        return removeQuestion(findQuestion(q));
+
     }
 
     @Override
     public int numberQuestions() {
-        return 0;
+        int count = 0;
+        for (Question q : questions)
+        { // Itera sobre toda a estutura de dados
+            if (q != null)
+            { // Incrementa o contador de cada vez que encontra uma questão
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
     public boolean isComplete() {
-        return false;
+        if (questions.length == 0)
+        { // Se não existirem questões, retorna falso
+            return false;
+        }
+        for (Question q : questions)
+        { // Itera sobre a estrutura de dados até que a condição se realize
+            if (!q.isDone())
+            { // Ao encontrar uma única questão incompleta, retorna falso
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public ITestStatistics getTestStatistics() {
-        return null;
+        return this.statistics = new TestStatistics(this.questions);
     }
 
     @Override
-    public boolean loadFromJSONFile(String var1) throws TestException {
-        return false;
+    public boolean loadFromJSONFile(String path) throws TestException {
+        Gson gson = new Gson();
+        Question[] fileQuestions = gson.fromJson(path, Question[].class);
+
+        if (fileQuestions.length == 0)
+        { // Se não existirem questões, retorna falso
+            return false;
+        }
+
+        for (Question q : fileQuestions)
+        { // Iterar sobre o novo vetor criado a partir do ficheiro JSON
+            /*
+            Nota: como o método 'addQuestion()' já faz o lançamento da exceção
+            'TestException()' quando não existe uma questão na posição em 
+            específico, não é necessário voltar a instânciar e a lançar
+            a mesma exceção.
+             */
+            if (!addQuestion(q))
+            { // Não sendo possível adicionar uma única questão, retorna falso
+                return false;
+            }
+            // Questão adicionada, prossegue para a próxima posição.
+        }
+        return true;
+    }
+
+    /**
+     * Override do método por defeito '.toString()' para permitir a definição de
+     * um formato personalizado.
+     *
+     * @return resultados do teste em formato String
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Gson gson = new Gson();
+        for (IQuestion q : this.questions)
+        {
+            builder.append("----------------\nQuestion:\n");
+            builder.append(gson.toJson(q));
+            builder.append('\n');
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Procura e devolve a posição de um elemento em {@link questions}.
+     *
+     * @param q elemento a ser encontrado
+     * @return posição em que se encontra o elemento encontrado ou -1 se não
+     * existir
+     */
+    public int findQuestion(IQuestion q) {
+        int pos = 0;
+        while (pos < questions.length)
+        { // Iterar sobre a estrutura de dados
+            if (questions[pos].equals(q))
+            { // Se encontrar um objeto igual ao parâmetro, retorna a posição
+                return pos;
+            }
+            // Senão, avança para a próxima posição
+            pos++;
+        }
+        return -1;
+    }
+
+    /**
+     * Elimina elementos vazios entre cada dois elementos consecutivos, a partir
+     * de uma posição inicial.<p>
+     * <b>Nota:</b> Este método não está a ser utilizado devido a como a
+     * estrutura de dados está a ser tratada. Isto é, de tal forma que não
+     * necessário o seu uso.
+     *
+     * @param pos posição inicial
+     */
+    public void trimData(int pos) {
+        int i = pos, j;
+        while (i < questions.length)
+        { // Iterar sobre toda a estrutura de dados
+            if (questions[i] == null)
+            { // Quando encontrar um elemento nulo
+                j = i;
+                while (j < questions.length - 1 && questions[j] == null)
+                { // Arrasta o elemento seguinte para a posição anterior até 
+                    // que mais nenhum elemento nulo exista
+                    questions[j] = questions[j + 1];
+                    j++;
+                }
+            }
+            i++;
+        }
     }
 }
