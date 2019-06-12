53a878f0d34127c31b3a893d629ad716ca4b208e
JoseBaltar <josebaltar_17@hotmail.com>
2019-06-12 14:53:18 +0100

    [Add] Adicionado o exemplo de utilização na classe com o método 'main'

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,44 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Nome: José Ribeiro Baltar
+ * Número: 8170212
+ * Turma: Turma 1
+ * 
+ * Nome: Rodrigo Alexandre Ferreira Coelho
+ * Número: 8170282
+ * Turma: Turma 1
  */
-
 package main;
 
+import controller.Test;
+import interfaces.exceptions.TestException;
+import views.TestWindow;
+
+/**
+ *
+ * @author Asus
+ */
 public class Main {
 
     public static void main(String[] args) {
-        
+        /* 
+        Código abaixo serve como exemplo de utilização
+         */
+        System.out.println("Início de Teste!");
+
+        // Carregar o teste
+        Test demoTest = new Test();
+
+        // Executar o teste na camada gráfica
+        TestWindow t = new TestWindow();
+        try {
+            t.startTest(demoTest);
+
+            // Obter os resultados do teste
+            System.out.println("Teste Efetuado!");
+            System.out.println(demoTest.toString());
+        } catch (TestException ex) {
+            //tratar exceção
+        }
     }
 
 }
